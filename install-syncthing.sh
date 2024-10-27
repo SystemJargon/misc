583a14f9c34e72fab77dcda02e3757cb55276dbc
SystemJargon <24641464+SystemJargon@users.noreply.github.com>
2024-10-28 09:57:19 +1300

    Create install-syncthing.sh

---
+++ install-syncthing.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get install curl
+curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
+echo "deb https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+sudo apt-get install apt-transport-https
+sudo apt-get install syncthing
